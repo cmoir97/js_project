642f3af686b4e2797994631233cc3742890041e5
Cameron Moir <cameronjmoir@gmail.com>
2019-11-02 14:06:19 +0000

    fetched launch data from api

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,28 +1,25 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="html">
+<div id="app">
+<h1>hi</h1>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  name:"app",
+  components:{},
+  data(){
+    return {
+      launches: []
+    }
+  },
+  mounted(){
+    fetch('https://api.spacexdata.com/v3/launches')
+    .then(res => res.json())
+    .then(data => console.log(data))
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 50px;
-}
+<style lang="css" scoped>
 </style>
