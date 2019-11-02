40abdb32a523da0511986c712c7871d598b2261c
Cameron Moir <cameronjmoir@gmail.com>
2019-11-02 14:56:13 +0000

    adds drop down bar to launches list

---
+++ client/src/components/LaunchDetail.vue
@@ -0,0 +1,25 @@
+<template lang="html">
+  <div v-if="launch" id="launchDetail">
+    <div id="detailWrapper">
+      <h2>{{ launch.mission_name }}</h2>
+      <div id="flexWrapper">
+        <div id="left">
+          <p>Launch date : {{ launch.launch_date_utc }}</p>
+          <p>Launch site: {{ launch.launch_site.site_name }}</p>
+          <p>Successful? {{ launch.launch_success }}</p>
+          <p>{{ launch.details }}</p>
+
+
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css" scoped>
+</style>
