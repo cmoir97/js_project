e68ea3f1003caf85aa81641d6c573088840f62c4
Cameron Moir <cameronjmoir@gmail.com>
2019-11-05 11:00:44 +0000

    populated seeds file #2

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -7,5 +7,10 @@ db.missions.insertMany([
     description: "Take high quality images of Jupiter.",
     budget: 20000000
   },
+  {
+    name: "ZsoltSat",
+    description: "Take high quality images of some Good Stuff",
+    budget: 50000000
+  }
 
 ])
