5bfc00676dba9d0112fa2626910274bbd1572a68
user <you@exampalex.com>
2019-11-06 10:06:57 +0000

    created chart data function

--- client/src/components/LaunchNationalityChart.vue
+++ client/src/components/LaunchNationalityChart.vue
@@ -19,16 +19,16 @@ export default {
   data () {
     return {
       chartData: [
-        ['Year', 'Sales', 'Expenses', 'Profit'],
-        ['2014', 1000, 400, 200],
-        ['2015', 1170, 460, 250],
-        ['2016', 660, 1120, 300],
-        ['2017', 1030, 540, 350]
+        ['Year', 'Sales'],
+        ['2014', 1000],
+        ['2015', 1170],
+        ['2016', 660],
+        ['2017', 1030]
       ],
       chartOptions: {
         chart: {
           title: 'Company Performance',
-          subtitle: 'Sales, Expenses, and Profit: 2014-2017',
+          subtitle: 'Sales, Expenses, and Profit: 2014-2017'
         }
       },
     }
